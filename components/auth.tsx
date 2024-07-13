0e1cff1045c354def09cd37806513eef64a867d5
ranadeep <ranadeepbashetty008@gmail.com>
2024-07-13 19:00:49 +0530

    first text as auth in auth component

--- components/auth.tsx
+++ components/auth.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Text, View } from 'react-native'
 
 export default function auth() {
   return (
     <div>
-      
+      <View>
+        <Text>Auth</Text>
+      </View>
     </div>
   )
 }
